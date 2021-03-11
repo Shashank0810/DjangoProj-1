1294351abb5c5758494725f196bf483118dc8e72
Shashank0810 <35419988+Shashank0810@users.noreply.github.com>
2021-03-11 12:18:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs"{
     bucket      = "terraformstatestore"
     prefix      = "dev"
-    credentials = "~/Downloads/codelabs-gcp1-96dd7b3643c9.json"
+    credentials = ${{ secrets.GOOGLE_CREDENTIALS }}
   }
 }
